d3fe76c45a3388527ca6aeca5c2083c54e4c4d39
Angel <angel.luis.leal.baron@gmail.com>
2021-03-27 19:20:44 +0100

    Added Market Homework.

---
+++ Market/Market/Market.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Market
+{
+    public class Market
+    {
+        /// <summary>
+        /// Realizes then market signdown process
+        /// </summary>
+        /// <param name="TotalOrder">Totla order</param>
+        /// <param name="MarketElements">List of <see cref="MarketElement"/></param>
+        /// <returns>The market signdown processed</returns>
+        public List<MarketElement> MarketSigndown(float TotalOrder, List<MarketElement> MarketElements)
+        {
+
+            return CalculateSignedLines(TotalOrder, MarketElements);
+
+        }
+
+        /// <summary>
+        /// Assign signed lines value in a List of <see cref="MarketElement"/>
+        /// </summary>
+        /// <param name="ratio">Ratio</param>
+        /// <param name="MarketElements">MarketElements</param>
+        /// <returns>List of <see cref="MarketElement"/> with assigned  signledlines</returns>
+        private List<MarketElement> AssignSignedLines(float ratio, List<MarketElement> MarketElements)
+        {
+            
+            foreach (MarketElement u in MarketElements)
+            {
+                u.SignedLine = u.WrittenLine * ratio;
+            }
+
+            return MarketElements;
+
+        }
+
+        /// <summary>
+        /// For a <paramref name="TotalOrder"/> returns the ratio
+        /// </summary>
+        /// <param name="TotalOrder">Total Order</param>
+        /// <param name="MarketElements">Ratio</param>
+        /// <returns>Ratio</returns>
+        private float GetRatio(float TotalOrder, List<MarketElement> MarketElements)
+        {
+            return (TotalOrder / MarketElements.Sum(p => p.WrittenLine));
+        }
+
+        /// <summary>
+        /// Calculate signed lines and retry the process if any MarketElement does not 
+        /// get its minimum share
+        /// </summary>
+        /// <param name="TotalOrder">Total order</param>
+        /// <param name="MarketElements">List of MarketElements</param>
+        /// <returns>List of MarketElements with their signed lines calculated</returns>
+        private List<MarketElement> CalculateSignedLines(float TotalOrder, List<MarketElement> MarketElements)
+        {
+
+            if (MarketElements.Count > 0)
+            {
+                float ratio = GetRatio(TotalOrder, MarketElements);
+
+                if (ratio <= 1)
+                {
+
+                    MarketElements = AssignSignedLines(ratio, MarketElements);
+
+                }
+
+                MarketElement failedItem = CheckMinimumsEarned(MarketElements);
+
+                List<MarketElement> failedItems = new List<MarketElement>();
+
+                while (!(failedItem is null) && MarketElements.Count > 0)
+                {
+
+                    failedItems.Add(failedItem);
+
+                    failedItem.SignedLine = failedItem.MinLine;
+
+                    MarketElements.Remove(failedItem);
+
+                    TotalOrder -= failedItem.SignedLine;
+                    
+                    MarketElements = AssignSignedLines(GetRatio(TotalOrder, MarketElements), MarketElements);
+
+                    failedItem = CheckMinimumsEarned(MarketElements);
+
+                }
+
+                if (MarketElements.Count == 0)
+                {
+                    throw new Exception("Failed configuration");
+                }
+                else
+                {
+                    MarketElements.AddRange(failedItems);
+                }
+            }
+
+            return MarketElements;
+
+        }
+
+        /// <summary>
+        /// Checks if every MarketElement has its minimum calculated
+        /// </summary>
+        /// <param name="MarketElements">MarketElements</param>
+        /// <returns>If an MarketElement (or more) has not its minimum, function
+        /// returns the first item which does not have its minimum</returns>
+        private MarketElement CheckMinimumsEarned(List<MarketElement> MarketElements)
+        {
+            return (MarketElements.Where(p => p.SignedLine < p.MinLine).FirstOrDefault());
+        }
+
+    }
+}
+
+
---
+++ Market/MarketTest/MarketTest.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarketSigndownTestProject
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class MarketSigndownTest
+    {
+        Market.Market market;
+        public MarketSigndownTest()
+        {
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        public void TestMarketSigndown_01_Empty()
+        {
+            market = new Market.Market();
+            List<Market.MarketElement> MarketElements = new List<Market.MarketElement>();
+            market.MarketSigndown(0, MarketElements);
+
+
+            Assert.IsTrue(MarketElements.Count == 0, "Returned MarketElements list is empty when imput list is empty too.");
+        }
+
+        public void TestMarketSigndown_02_Simple_Example_NoMins1()
+        {
+
+            // test the trivial case.
+            market = new Market.Market();
+            List<Market.MarketElement> listUWs = new List<Market.MarketElement>
+            {
+                new Market.MarketElement(10, "Element 1", 40, 30, 0),
+                new Market.MarketElement(20, "Element 2", 60, 20, 0)
+            };
+
+            listUWs = market.MarketSigndown(100, listUWs);
+
+            AssertSignedLine(listUWs, "Element 1", 40f);
+            AssertSignedLine(listUWs, "Element 2", 60f);
+        }
+
+        public void TestMarketSigndown_03_Simple_Example_NoMins_TotalOrderNot100()
+        {
+
+            // 
+            market = new Market.Market();
+            List<Market.MarketElement> listUWs = new List<Market.MarketElement>
+            {
+                new Market.MarketElement(10, "Element 1", 30, 0, 0),
+                new Market.MarketElement(20, "Element 2", 30, 0, 0)
+            };
+
+            listUWs = market.MarketSigndown(60, listUWs);
+
+            AssertSignedLine(listUWs, "Element 1", 30f);
+            AssertSignedLine(listUWs, "Element 2", 30f);
+        }
+
+        public void TestMarketSigndown_04_Simple_Example_NoMins2()
+        {
+
+            // test the trivial case.
+            market = new Market.Market();
+            List<Market.MarketElement> listUWs = new List<Market.MarketElement>
+            {
+                new Market.MarketElement(10, "Element 1", 80, 10, 0),
+                new Market.MarketElement(20, "Element 2", 40, 10, 0),
+                new Market.MarketElement(30, "Element 3", 40, 20, 0)
+            };
+
+            listUWs = market.MarketSigndown(100, listUWs);
+
+            AssertSignedLine(listUWs, "Element 1", 50f);
+            AssertSignedLine(listUWs, "Element 2", 25f);
+            AssertSignedLine(listUWs, "Element 3", 25f);
+        }
+
+        public void TestMarketSigndown_05_Simple_Example_NoMins3()
+        {
+
+            // test the trivial case.
+            market = new Market.Market();
+            List<Market.MarketElement> listUWs = new List<Market.MarketElement>
+            {
+                new Market.MarketElement(10, "Element 1", 80, 0, 0),
+                new Market.MarketElement(20, "Element 2", 40, 0, 0),
+                new Market.MarketElement(30, "Element 3", 40, 0, 0)
+            };
+
+            listUWs = market.MarketSigndown(50, listUWs);
+
+            AssertSignedLine(listUWs, "Element 1", 25f);
+            AssertSignedLine(listUWs, "Element 2", 12.5f);
+            AssertSignedLine(listUWs, "Element 3", 12.5f);
+        }
+
+        public void TestMarketSigndown_06_Mins1_AsPerExcel_Example()
+        {
+
+            // test the trivial case.
+            market = new Market.Market();
+            List<Market.MarketElement> listUWs = new List<Market.MarketElement>
+            {
+                new Market.MarketElement(10, "Element 1", 80, 10, 0),
+                new Market.MarketElement(20, "Element 2", 90, 10, 0),
+                new Market.MarketElement(30, "Element 3", 40, 20, 0)
+            };
+
+            listUWs = market.MarketSigndown(100, listUWs);
+
+            AssertSignedLine(listUWs, "Element 1", 37.65f);
+            AssertSignedLine(listUWs, "Element 2", 42.35f);
+            AssertSignedLine(listUWs, "Element 3", 20f);
+        }
+
+        public void TestMarketSigndown_07_Mins2_2DecimalPlaces_SameResultsAsTest06()
+        {
+
+            // test the trivial case.
+            market = new Market.Market();
+            List<Market.MarketElement> listUWs = new List<Market.MarketElement>
+            {
+                new Market.MarketElement(10, "Element 1", 80, 20, 0),
+                new Market.MarketElement(20, "Element 2", 90, 40, 0),
+                new Market.MarketElement(30, "Element 3", 40, 20, 0)
+            };
+
+            listUWs = market.MarketSigndown(100, listUWs);
+
+            AssertSignedLine(listUWs, "Element 1", 37.65f);
+            AssertSignedLine(listUWs, "Element 2", 42.35f);
+            AssertSignedLine(listUWs, "Element 3", 20f);
+        }
+
+        public void TestMarketSigndown_08_Mins3_UW2GetsMinSoTheRestForUW1()
+        {
+
+            // test the trivial case.
+            market = new Market.Market();
+            List<Market.MarketElement> listUWs = new List<Market.MarketElement>
+            {
+                new Market.MarketElement(10, "Element 1", 80, 20, 0),
+                new Market.MarketElement(20, "Element 2", 90, 70, 0)
+            };
+
+            listUWs = market.MarketSigndown(100, listUWs);
+
+            AssertSignedLine(listUWs, "Element 1", 30f);
+            AssertSignedLine(listUWs, "Element 2", 70f);
+        }
+
+
+        public void TestMarketSigndown_09_Mins4_SortUWsBySequenceNumber()
+        {
+
+            // test the trivial case.
+            market = new Market.Market();
+            List<Market.MarketElement> listUWs = new List<Market.MarketElement>
+            {
+                new Market.MarketElement(10, "Element 1", 80, 20, 0),
+                new Market.MarketElement(20, "Element 2", 90, 50, 0),
+                new Market.MarketElement(30, "Element 3", 40, 20, 0)
+            };
+
+            listUWs = market.MarketSigndown(100, listUWs);
+
+            AssertSignedLine(listUWs, "Element 1", 30f);
+            AssertSignedLine(listUWs, "Element 2", 50f);
+            AssertSignedLine(listUWs, "Element 3", 20f);
+        }
+
+        public void TestMarketSigndown_10_Mins5_MinimumsAreTheSolution()
+        {
+
+            // test the trivial case.
+            market = new Market.Market();
+            List<Market.MarketElement> listUWs = new List<Market.MarketElement>
+            {
+                new Market.MarketElement(10, "Element 1", 80, 34, 0),
+                new Market.MarketElement(20, "Element 2", 90, 33, 0),
+                new Market.MarketElement(30, "Element 3", 40, 33, 0)
+            };
+
+            listUWs = market.MarketSigndown(100, listUWs);
+
+            AssertSignedLine(listUWs, "Element 1", 34f);
+            AssertSignedLine(listUWs, "Element 2", 33f);
+            AssertSignedLine(listUWs, "Element 3", 33f);
+        }
+
+        public void AssertSignedLine(List<Market.MarketElement> listUWs, string name, float signedLine)
+        {
+            Market.MarketElement u1 = listUWs.Find(u => u.Name.Equals(name));
+            Assert.IsNotNull(u1, " MarketElement " + name + " was not found!");
+
+            Assert.AreEqual(u1.SignedLine, signedLine, 0.1f, " expected signed line for " + name + " is " + signedLine.ToString() + ". It was " + u1.SignedLine.ToString());
+        }
+    }
+}
